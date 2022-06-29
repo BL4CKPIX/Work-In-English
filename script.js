f5e4f6d98f24c0f5359a28d77ef26b36a8df6431
JØRDAN SANTØS <jordansantos-11@outlook.com>
2022-06-29 20:07:40 -0300

    complet desafio

---
+++ script.js
@@ -0,0 +1,26 @@
+
+let Comedy = [{
+    title:'Ted',
+    director: 'Seth MacFarlane',
+    year:'21 de September de 2012',
+
+}]
+console.log(Comedy)
+let Action = [{
+    title: 'Resident Evil 2: Apocalypse',
+    director: 'Alexander Witt',
+    year: '8 de October de 2004 ',
+}]
+console.log(Action)
+let Fantasy = [{
+    title: 'The last witch Hunter',
+    director: 'Breck Eisner',
+    year: '29 de October de 2015',
+}]
+console.log(Fantasy)
+
+if (Comedy = 'Ted'){
+    console.log(`MY FAVORIT MOVIE IS ${Comedy}`)
+}else{
+    console.log('NOT EVEN 5 STARS')
+}
